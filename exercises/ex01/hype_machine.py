e4c1b592f59a84215df352964db6c7e3ce03e60c
Shreyas Gotur <sgotur@email.unc.edu>
2021-08-30 15:28:42 -0400

    Progress on Exercise 1

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -4,5 +4,5 @@ __author__ = "730391001"
 
 name: str = input("What is your name? ")
 print(name + " is such a unique name! ")
-print(name + ", you have a purpose in this world so keep working to fulfil that purpose!")
+print("You have a purpose in this world so keep working to fulfil that purpose " + name)
 print("Don't forget " + name + ", a smile is the best thing you can wear, so keep smiling!")
