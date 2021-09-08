f20cbb56e3626380731b653b7b5984b315743756
Shreyas Gotur <sgotur@email.unc.edu>
2021-09-08 19:23:02 -0400

    Progress on Exercise 2

--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -5,8 +5,10 @@ __author__ = "730391001"
 
 b: str = str(input("What beat do you want to repeat? "))
 n: int = int(input("How many times do you want to repeat it? "))
+s: str = ""
 if n <= 0:
     print("No beat...")
 while n > 0:
-    print(str(b + " ") * n)
-    n = n - n
+    s = s + b + " "
+    n = n - 1
+print(s)
