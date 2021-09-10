77fbe81825143abd742d271e87c141aeeca77f6d
Shreyas Gotur <sgotur@email.unc.edu>
2021-09-09 22:06:23 -0400

    Progress on Exercise 2

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -4,12 +4,10 @@ __author__ = "730391001"
 
 letter = input("What letter do you want to search for? ")
 word = input("Enter a word: ")
-
 count = 0
 i = 0
 while(i < len(word)):
     if(word[i] == letter):
         count = count + 1
     i = i + 1
-
-print(count)
+print("Count:" + " " + str(count))
--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -6,9 +6,12 @@ __author__ = "730391001"
 b: str = str(input("What beat do you want to repeat? "))
 n: int = int(input("How many times do you want to repeat it? "))
 s: str = ""
-if n <= 0:
+if n == 1:
     print("No beat...")
 while n > 0:
-    s = s + b + " "
+    if n == 1:
+        s = s + b
+    else:
+        s = s + b + " "
     n = n - 1
 print(s)
